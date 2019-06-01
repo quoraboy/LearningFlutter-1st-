a79a73230ace96eb78cc48e8c7b1cca22e8cdf83
quoraboy <sachin.s.pathak1998@gmail.com>
2019-06-01 14:31:35 +0530

    Worked on the UI of 2nd activity of the app

--- flutter_course_first_project/lib/pages/product_page.dart
+++ flutter_course_first_project/lib/pages/product_page.dart
@@ -6,15 +6,17 @@ class pro extends StatelessWidget {
     // TODO: implement build
     return Scaffold(
       appBar: AppBar(title: Text('Product details')),
-      body: Column(
-        children: <Widget>[
-          Text('Details'),
-          RaisedButton(
+      body:Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[Image.asset('lib/assets/food.jpg'),
+         Container(padding: EdgeInsets.all(10.0) ,child: Text('Details')) ,
+          Container(padding: EdgeInsets.all(5.0),child: RaisedButton(
             child: Text('back'),
             onPressed: () => Navigator.pop(context),
-          )
+          ))
+           
         ],
-      ),
+      ) ,
     );
   }
 }
